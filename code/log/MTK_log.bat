efd41e7663d9793d38987c4b22cda9ef15357db7
zxianbing <13338276991@163.com>
2018-11-01 18:56:48 +0800

    解决如果当前目录下没有log的目录会报找不到路径问题，现在在导出log钱会先判断当前目录下有没有这个目录，没有这个目录就创建，有这个目录就不创建。

--- code/log/MTK_log.bat
+++ code/log/MTK_log.bat
@@ -8,7 +8,14 @@ set mounth=%DATE:~5,2%
 set data=%DATE:~8,2%
 set minute=%time:~3,2%
 set sec=%time:~6,2%
-adb pull storage/emulated/0/mtklog C:/Users/Administrator/Desktop/log/log/%name%-%year%-%mounth%-%data%-%minute%-%sec%
+set Pan=.\log 
+ if exist %Pan% (         
+        rem 目录d:\<span style="font-family: Arial, Helvetica, sans-serif;">MIS</span>已存在，无需创建  
+    ) else (  
+        rem 创建d:\MIS   
+        mkdir %Pan%
+    )  
+adb pull storage/emulated/0/mtklog .\log\%name%-%year%-%mounth%-%data%-%minute%-%sec%
 pause|echo pull mtklog sucessful !
 
 
